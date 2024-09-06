faac56ccc02bb8fac1c8c5c193581ff0ce1cb1a9
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-06 13:17:34 +0200

    Update Postazione.java

--- src/main/java/com/example/u5d5project/entities/Postazione.java
+++ src/main/java/com/example/u5d5project/entities/Postazione.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Entity
 public class Postazione {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
