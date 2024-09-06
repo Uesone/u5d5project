1e6734f569047669c9f0ed9df03a158030d763b5
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-06 13:16:49 +0200

    Creazione Entities
    Edificio, postazione

--- src/main/java/com/example/u5d5project/entities/Edificio.java
+++ src/main/java/com/example/u5d5project/entities/Edificio.java
@@ -1,4 +1,55 @@
 package com.example.u5d5project.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Edificio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String citta;
+    @OneToMany(mappedBy = "edificio", cascade = CascadeType.ALL)
+    private List<Postazione> postazioni;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public List<Postazione> getPostazioni() {
+        return postazioni;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public void setPostazioni(List<Postazione> postazioni) {
+        this.postazioni = postazioni;
+    }
+
+    public Edificio(Long id, String nome, String citta, List<Postazione> postazioni) {
+        this.id = id;
+        this.nome = nome;
+        this.citta = citta;
+        this.postazioni = postazioni;
+    }
 }
--- src/main/java/com/example/u5d5project/entities/Postazione.java
+++ src/main/java/com/example/u5d5project/entities/Postazione.java
@@ -1,4 +1,24 @@
 package com.example.u5d5project.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Postazione {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String codice;
+    private String descrizione;
+    private String tipo;
+    private int numeroMassimoOccupanti;
+
+    @ManyToOne
+    @JoinColumn(name = "edificio_id")
+    private Edificio edificio;
+
+
 }
